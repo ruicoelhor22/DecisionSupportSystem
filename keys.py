74c039c6c28ead0b693f249ce5ba7d9931528e2e
Rui Pedro Fernandes Coelho <58275291+ruicoelhor22@users.noreply.github.com>
2024-06-20 13:10:26 +0100

    Delete keys.py

--- keys.py
+++
@@ -1,6 +0,0 @@
-keys = { 
-    'SOLVER': "e5sNiIgfdMeMR4ZE8aus9BBI-q_qextV2IWCMduT1BMeMSUOuEuHuo20wEIJBMB7", 
-    'RESPONSE': "b264d3d2-64db-a24c-86bb-790f3f0b354c",
-    'API_KEY': "AIzaSyBPRHFYBEr5hIcjNmzPiCdSZf75gXXq8JI", 
-    'CX': "f15272a6f558d4ea5"
-} 
